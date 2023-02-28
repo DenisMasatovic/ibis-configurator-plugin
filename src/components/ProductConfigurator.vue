491327bb2201ebca144a1a607e0062d9b46b2998
Denis Masatovic <masatovic.denis@gmail.com>
2023-02-28 15:34:21 +0100

    build main shape with option list and fonts

---
+++ src/components/ProductConfigurator.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="main">
+    <div class="optionListWrapper">
+      <div class="optionList">
+        <ul>
+          <li>
+            <input type="radio" id="option1" name="options" value="option1" />
+            <label for="option1">Stange</label>
+          </li>
+          <li>
+            <input type="radio" id="option2" name="options" value="option2" />
+            <label for="option2">Zuschnitt</label>
+          </li>
+          <li>
+            <input type="radio" id="option3" name="options" value="option3" />
+            <label for="option3">Gerader Schnitt</label>
+          </li>
+          <li>
+            <input type="radio" id="option4" name="options" value="option4" />
+            <label for="option4">Leerrahmen</label>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.main {
+  width: 818px;
+  height: 216px;
+  left: 313px;
+  top: 852px;
+
+  background: #ffffff;
+  box-shadow: 0px 8px 37px rgba(64, 64, 64, 0.1);
+  border-radius: 25px;
+  margin: auto;
+
+  .optionListWrapper {
+    width: 30%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .optionList {
+      ul {
+        list-style-type: none;
+        display: flex;
+        flex-flow: column;
+        gap: 20px;
+
+        li {
+          display: flex;
+          gap: 10px;
+        }
+      }
+    }
+  }
+}
+</style>
