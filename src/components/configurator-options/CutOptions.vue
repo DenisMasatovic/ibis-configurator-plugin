c95636f684bf290dffc028bfcea61644711898fc
Denis Masatovic <masatovic.denis@gmail.com>
2023-03-05 13:15:29 +0100

    add new files

---
+++ src/components/configurator-options/CutOptions.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="optionsWrapper">
+    <q-btn-dropdown color="primary" label="Dropdown Button">
+      <q-list>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Photos</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Videos</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section>
+            <q-item-label>Articles</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped lang="scss">
+@import "../../assets/sass/style.scss";
+.optionsWrapper {
+    .q-btn:before{
+        box-shadow: none;
+    }
+    .text-white{
+        color: $ibis-black !important;
+    }
+}
+</style>
---
+++ src/components/configurator/ProductConfigurator.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="main">
+    <div class="optionListWrapper">
+      <div class="optionList">
+        <ul>
+          <li>
+            <input
+              type="radio"
+              id="option1"
+              name="options"
+              value="option1"
+              checked="true"
+            />
+            <label for="option1">Stange</label>
+          </li>
+          <li>
+            <input type="radio" id="option2" name="options" value="option2" />
+            <label for="option2">Zuschnitt</label>
+          </li>
+          <li>
+            <input type="radio" id="option3" name="options" value="option3" />
+            <label for="option3">Gerader Schnitt</label>
+          </li>
+          <li>
+            <input type="radio" id="option4" name="options" value="option4" />
+            <label for="option4">Leerrahmen</label>
+          </li>
+        </ul>
+      </div>
+      <div class="categoryOptionsWrapper">
+        <CutOptions> </CutOptions>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const firebaseHandler = require("@/utility/FirebaseHandler.js");
+
+import CutOptions from "../configurator-options/CutOptions.vue";
+export default {
+  components: {
+    CutOptions,
+  },
+
+  mounted() {
+    this.fetchAllProducts();
+  },
+
+  methods: {
+    async fetchAllProducts() {
+      console.log(await firebaseHandler.getAllProducts());
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "../../assets/sass/style.scss";
+
+.main {
+  width: 818px;
+  height: 216px;
+  left: 313px;
+  top: 852px;
+
+  background: #ffffff;
+  box-shadow: 0px 8px 37px rgba(64, 64, 64, 0.1);
+  border-radius: 25px;
+  margin: auto;
+
+  .optionListWrapper {
+    width: 95%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .optionList {
+      ul {
+        list-style-type: none;
+        display: flex;
+        flex-flow: column;
+        gap: 17px;
+
+        li {
+          display: flex;
+          font-weight: 700;
+          align-items: center;
+
+          label {
+            margin-top: 3px;
+            font-size: $font-size-big;
+          }
+
+          input[type="radio"] {
+            appearance: none;
+            background: $ibis-grey-middle;
+            border-radius: 50%;
+            width: 25px;
+            height: 25px;
+            margin-right: 10px;
+          }
+          input[type="radio"]:not(:checked) + label {
+            color: $ibis-grey-middle;
+          }
+
+          input[type="radio"]:checked {
+            background-color: $ibis-red;
+            border-color: $ibis-red;
+          }
+        }
+      }
+    }
+    .categoryOptionsWrapper{
+      height: 40%;
+    }
+  }
+}
+</style>
